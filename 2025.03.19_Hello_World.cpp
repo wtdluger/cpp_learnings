59b3c2af0ef9101e901920835ec5d24bf6cfd15d
wtdluger <wtdluger@gmail.com>
2025-03-19 18:10:38 -0700

    Update Hello World: different print functions

--- 2025.03.19_Hello_World.cpp
+++ 2025.03.19_Hello_World.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
+// declare functions other than main
+void print_string(std::string str) {
+    std::cout << str << std::endl;
+}
+
 int main() {
-    std::cout << "Hello, C++ from Colab using cout" << std::endl;
-    printf("Hello, C++ from Colab using printf");
+    std::string hw = "Hello, World!";
+    std::cout << hw << " using std::cout"<< std::endl;
+    std::printf("%s using printf \n", hw.c_str() );
+    print_string(hw + " using user defined function");
     return 0;
 }
