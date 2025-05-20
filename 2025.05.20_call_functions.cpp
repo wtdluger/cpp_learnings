93c3528b199fe334bc7687cc637e9d33a9205847
wtdluger <wtdluger@gmail.com>
2025-05-19 21:11:02 -0700

    Upload wtdlib & tutorial
    wtdlib.h, wtdlib.cpp, 2025.05.20_call_functions.cpp to demonstrate calling wtdlib.

---
+++ 2025.05.20_call_functions.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include "wtdlib.h"
+using namespace std;
+
+int main() {
+    int i = 1000;
+    vector v = {1, 2, 3, 4, 5};
+    cout << "Hello" << endl;
+    timer(i);
+    cout << "Good bye" << endl;
+    // printVector(v);
+    return 0;
+}
+
+/*
+    linux command to compile linked files
+    g++ wtdlib.cpp 2025.05.20_call_functions.cpp -o call_functions
+
+    linux command to run
+    ./call_functions
+*/
