5b6713f3aad52a67491b4439c933c277733642dc
wtdluger <wtdluger@gmail.com>
2025-07-10 09:18:05 -0700

    Added additional variables

--- 2025.06.18_memory_allocation.cpp
+++ 2025.06.18_memory_allocation.cpp
@@ -15,18 +15,35 @@ int main()
     s_dynamic_reassigned1 = "Foo";
     std::string s_dynamic_reassigned2 = "Foo";
     s_dynamic_reassigned2 = "Foo Bar";
+    std::string long_string = "abcdefghijklmnopqrstuvwxy1234567890z";
 
     cout << "sizeof c-style 50 char unused string: " 
         << sizeof(c_fifty_unused) << endl;
+    cout << "sizeof its reference: " << sizeof(&c_fifty_unused) << endl << endl;
+    
     cout << "sizeof c-style 50 char string,"
         " 7 chars utilized: " << sizeof(c_fifty_used) << endl;
+    cout << "sizeof its reference: " << sizeof(&c_fifty_used) << endl << endl;
+    
     cout << "sizeof c++ string array, 50 elements: " << sizeof(s_fifty) << endl;
+    cout << "sizeof its reference: " << sizeof(&s_fifty) << endl << endl;
+    
     cout << "sizeof c++ string unused: " << sizeof(s_unused) << endl;
+    cout << "sizeof its reference: " << sizeof(&s_unused) << endl << endl;
+    
     cout << "sizeof c++ string, 7 chars long: " << sizeof(s_dynamic_assigned) << endl;
+    cout << "sizeof its reference: " << sizeof(&s_dynamic_assigned) << endl << endl;
+    
     cout << "sizeof c++ string, defined with 7 chars, reassigned 3 chars: " <<
         sizeof(s_dynamic_reassigned1) << endl;
+    cout << "sizeof its reference: " << sizeof(&s_dynamic_reassigned1) << endl << endl;
+    
     cout << "sizeof c++ string, defined with 3 chars, reassigned 7 chars: " <<
         sizeof(s_dynamic_reassigned2) << endl;
-    
+    cout << "sizeof its reference: " << sizeof(&s_dynamic_reassigned2) << endl << endl;
+
+    cout << "sizeof c++ string, 36 chars long: " << sizeof(long_string) << endl;
+    cout << "sizeof its reference: " << sizeof(&long_string) << endl;
+    cout << long_string << endl;
     return 0;
 }
